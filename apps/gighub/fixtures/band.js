471f2c488bec93e28851fe08a4b0393fe50eb732
Matt Mendell <matthew.mendell@gmail.com>
2011-05-02 21:02:57 -0700

    add initial Band model

---
+++ apps/gighub/fixtures/band.js
@@ -0,0 +1,29 @@
+// ==========================================================================
+// Project:   Gighub.Band Fixtures
+// Copyright: ©2011 My Company, Inc.
+// ==========================================================================
+/*globals Gighub */
+
+sc_require('models/band');
+
+Gighub.Band.FIXTURES = [
+
+    {
+        guid: 1,
+        name: "Wires in the Walls",
+        location: "Los Angeles, CA"
+    },
+
+    {
+        guid: 2,
+        name: "The Limousines",
+        location: "Campbell, CA"
+    },
+
+    {
+        guid: 3,
+        name: "30 Seconds to Mars"
+        location: "Los Angeles, CA"
+    }
+
+];
---
+++ apps/gighub/models/band.js
@@ -0,0 +1,21 @@
+// ==========================================================================
+// Project:   Gighub.Band
+// Copyright: ©2011 My Company, Inc.
+// ==========================================================================
+/*globals Gighub */
+
+/** @class
+
+  (Document your Model here)
+
+  @extends SC.Record
+  @version 0.1
+*/
+Gighub.Band = SC.Record.extend(
+/** @scope Gighub.Band.prototype */ {
+
+    name: SC.Record.attr(String),
+    location: SC.Record.attr(String),
+    logo: SC.Record.attr(String)
+
+}) ;
