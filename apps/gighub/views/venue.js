abaefb5409c3dfe2e8dcfdefcba59eccbbc7f9ca
Matt Mendell <matthew.mendell@gmail.com>
2011-05-15 20:41:30 -0700

    clean up the view
    use bindings correctly!

--- apps/gighub/views/venue.js
+++ apps/gighub/views/venue.js
@@ -12,22 +12,24 @@ Gighub.VenueView = SC.View.extend(
 
     layout: { top: 36 },
 
-    contentBinding: 'Gighub.venueController.content',
-    displayDidChange: function() {
-        this.set('layerNeedsUpdate', YES);
-    }.observes('content'),
+    name: '',
+    location: '',
+    website: '',
+
+    nameBinding: 'Gighub.venueController.name',
+    locationBinding: 'Gighub.venueController.location',
+    websiteBinding: 'Gighub.venueController.website',
+    displayProperties: ['name', 'location', 'website'],
 
     classNames: ['venue'],
 
     render: function(context, firstTime) {
-        var content = Gighub.venueController.get('content');
-        var name = content.get('name');
-        var location = content.get('location');
-        var website = content.get('website');
-
-        context = context.begin('div').addClass('v-name').push(name).end();
-        context = context.begin('div').addClass('v-location').push(location).end();
-        context = context.begin('a').addClass('v-website').attr('href', website).push('website').end();
+        context = context.begin('div').addClass('v-name')
+                         .push(this.get('name')).end();
+        context = context.begin('div').addClass('v-location')
+                         .push(this.get('location')).end();
+        context = context.begin('a').addClass('v-website').attr('href', this.get('website'))
+                         .push('website').end();
 
         sc_super();
     }
@@ -37,19 +39,17 @@ Gighub.VenueView = SC.View.extend(
 Gighub.RatingView = SC.View.extend({
     layout: { top: 150, height: 50, width: 250 },
 
-    contentBinding: 'Gighub.venueController.content',
-    displayDidChange: function() {
-        this.set('layerNeedsUpdate', YES);
-    }.observes('content'),
+    rating: 0,
+
+    ratingBinding: 'Gighub.venueController.rating',
+    displayProperties: ['rating'],
 
     render: function(context) {
-        var content = Gighub.venueController.get('content');
-        var rating = content.get('rating');
 
         // create stars
         for (i = 1; i < 6; i += 1) {
             var star_url = 'http://etc.usf.edu/clipart/37700/37717/05-star_37717_md.gif';
-            if (i <= rating) {
+            if (i <= this.get('rating')) {
                 star_url = 'http://www.cs.uwaterloo.ca/~wics/images/star.jpg';
             }
             context = context.begin('span').addClass('v-star');
