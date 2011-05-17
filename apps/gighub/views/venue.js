5749fbd9c70fb65a3df4291af5847d62d6041db2
Matt Mendell <matthew.mendell@gmail.com>
2011-05-17 08:17:56 -0700

    change star color on mouse over

--- apps/gighub/views/venue.js
+++ apps/gighub/views/venue.js
@@ -40,27 +40,174 @@ Gighub.VenueView = SC.View.extend(SC.ContentDisplay,
 
 });
 
-Gighub.RatingView = SC.View.extend({
-    layout: { top: 150, height: 50, width: 250 },
+Gighub.StarView1 = SC.ImageView.extend({
 
+    layout: { height: 50, width: 50 },
+
+    num: 1,
     rating: 0,
 
     ratingBinding: 'Gighub.venueController.rating',
     displayProperties: ['rating'],
 
-    render: function(context) {
+    star: '',
+    value: 'http://www.cs.uwaterloo.ca/~wics/images/star.jpg',
 
-        // create stars
-        for (i = 1; i < 6; i += 1) {
-            var star_url = 'http://etc.usf.edu/clipart/37700/37717/05-star_37717_md.gif';
-            if (i <= this.get('rating')) {
-                star_url = 'http://www.cs.uwaterloo.ca/~wics/images/star.jpg';
-            }
-            context = context.begin('span').addClass('v-star');
-            context = context.begin('img').attr('src', star_url).addClass('stretch').end();
-            context = context.end();
+    updateValue: function() {
+        if (this.get('rating') >= this.get('num')) {
+            this.set('value', 'http://www.cs.uwaterloo.ca/~wics/images/star.jpg');
         }
+        else {
+            this.set('value', 'http://etc.usf.edu/clipart/37700/37717/05-star_37717_md.gif');
+        }
+    }.observes('rating'),
 
-        sc_super();
+    mouseEntered: function() {
+        this.set('star', this.get('value'));
+        this.set('value', 'http://www1.pgcps.org/uploadedImages/Schools_and_Centers/Middle_Schools/G_James_Gholson/blue%20star.jpg');
+    },
+
+    mouseExited: function() {
+        this.set('value', this.get('star'));
     }
+});
+
+Gighub.StarView2 = SC.ImageView.extend({
+
+    layout: { left: 50, height: 50, width: 50 },
+
+    num: 2,
+    rating: 0,
+
+    ratingBinding: 'Gighub.venueController.rating',
+    displayProperties: ['rating'],
+
+    star: '',
+    value: 'http://www.cs.uwaterloo.ca/~wics/images/star.jpg',
+
+    updateValue: function() {
+        if (this.get('rating') >= this.get('num')) {
+            this.set('value', 'http://www.cs.uwaterloo.ca/~wics/images/star.jpg');
+        }
+        else {
+            this.set('value', 'http://etc.usf.edu/clipart/37700/37717/05-star_37717_md.gif');
+        }
+    }.observes('rating'),
+
+    mouseEntered: function() {
+        this.set('star', this.get('value'));
+        this.set('value', 'http://www1.pgcps.org/uploadedImages/Schools_and_Centers/Middle_Schools/G_James_Gholson/blue%20star.jpg');
+    },
+
+    mouseExited: function() {
+        this.set('value', this.get('star'));
+    }
+});
+
+Gighub.StarView3 = SC.ImageView.extend({
+
+    layout: { left: 100, height: 50, width: 50 },
+
+    num: 3,
+    rating: 0,
+
+    ratingBinding: 'Gighub.venueController.rating',
+    displayProperties: ['rating'],
+
+    star: '',
+    value: 'http://www.cs.uwaterloo.ca/~wics/images/star.jpg',
+
+    updateValue: function() {
+        if (this.get('rating') >= this.get('num')) {
+            this.set('value', 'http://www.cs.uwaterloo.ca/~wics/images/star.jpg');
+        }
+        else {
+            this.set('value', 'http://etc.usf.edu/clipart/37700/37717/05-star_37717_md.gif');
+        }
+    }.observes('rating'),
+
+    mouseEntered: function() {
+        this.set('star', this.get('value'));
+        this.set('value', 'http://www1.pgcps.org/uploadedImages/Schools_and_Centers/Middle_Schools/G_James_Gholson/blue%20star.jpg');
+    },
+
+    mouseExited: function() {
+        this.set('value', this.get('star'));
+    }
+});
+
+Gighub.StarView4 = SC.ImageView.extend({
+
+    layout: { left: 150, height: 50, width: 50 },
+
+    num: 4,
+    rating: 0,
+
+    ratingBinding: 'Gighub.venueController.rating',
+    displayProperties: ['rating'],
+
+    star: '',
+    value: 'http://www.cs.uwaterloo.ca/~wics/images/star.jpg',
+
+    updateValue: function() {
+        if (this.get('rating') >= this.get('num')) {
+            this.set('value', 'http://www.cs.uwaterloo.ca/~wics/images/star.jpg');
+        }
+        else {
+            this.set('value', 'http://etc.usf.edu/clipart/37700/37717/05-star_37717_md.gif');
+        }
+    }.observes('rating'),
+
+    mouseEntered: function() {
+        this.set('star', this.get('value'));
+        this.set('value', 'http://www1.pgcps.org/uploadedImages/Schools_and_Centers/Middle_Schools/G_James_Gholson/blue%20star.jpg');
+    },
+
+    mouseExited: function() {
+        this.set('value', this.get('star'));
+    }
+});
+
+Gighub.StarView5 = SC.ImageView.extend({
+
+    layout: { left: 200, height: 50, width: 50 },
+
+    num: 5,
+    rating: 0,
+
+    ratingBinding: 'Gighub.venueController.rating',
+    displayProperties: ['rating'],
+
+    star: '',
+    value: 'http://www.cs.uwaterloo.ca/~wics/images/star.jpg',
+
+    updateValue: function() {
+        if (this.get('rating') >= this.get('num')) {
+            this.set('value', 'http://www.cs.uwaterloo.ca/~wics/images/star.jpg');
+        }
+        else {
+            this.set('value', 'http://etc.usf.edu/clipart/37700/37717/05-star_37717_md.gif');
+        }
+    }.observes('rating'),
+
+    mouseEntered: function() {
+        this.set('star', this.get('value'));
+        this.set('value', 'http://www1.pgcps.org/uploadedImages/Schools_and_Centers/Middle_Schools/G_James_Gholson/blue%20star.jpg');
+    },
+
+    mouseExited: function() {
+        this.set('value', this.get('star'));
+    }
+});
+
+Gighub.RatingView = SC.View.extend({
+    layout: { top: 150, height: 50, width: 250 },
+
+    childViews: 'star1 star2 star3 star4 star5'.w(),
+
+    star1: Gighub.StarView1,
+    star2: Gighub.StarView2,
+    star3: Gighub.StarView3,
+    star4: Gighub.StarView4,
+    star5: Gighub.StarView5
 });
