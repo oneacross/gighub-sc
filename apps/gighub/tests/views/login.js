d71a47d0acb78696593e170607e915a77b736ae9
Matt Mendell <matthew.mendell@gmail.com>
2011-09-04 10:48:11 -0700

    add testing on SC.Validator

--- apps/gighub/tests/views/login.js
+++ apps/gighub/tests/views/login.js
@@ -6,10 +6,34 @@
 
 module("Gighub.LoginView");
 
-// TODO: Replace with real unit test for Gighub.LoginView
 test("test description", function() {
-  var expected = "test";
-  var result   = "test";
-  equals(result, expected, "test should equal test");
+    var expected = "test";
+    var result   = "test";
+    equals(result, expected, "test should equal test");
 });
 
+test("username must not be empty", function() {
+    var view = Gighub.LoginView.create();
+    view.username.field.set('value', '');
+    validator = SC.Validator.NotEmpty.create();
+    equals(validator.validate(undefined, view.username.field), NO);
+});
+
+test("username can be frank", function() {
+    var view = Gighub.LoginView.create();
+    view.username.field.set('value', 'frank');
+    validator = SC.Validator.NotEmpty.create();
+    equals(validator.validate(undefined, view.username.field), YES);
+});
+
+test("field has a fieldValue field", function() {
+    var view = Gighub.LoginView.create();
+    view.username.field.set('value', 'frank');
+    equals(view.username.field.get('fieldValue'), 'frank');
+});
+
+test("field value and a fieldValue are the same", function() {
+    var view = Gighub.LoginView.create();
+    view.username.field.set('value', 'frank');
+    equals(view.username.field.get('fieldValue'), view.username.field.get('value'));
+});
