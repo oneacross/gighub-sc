a39956774964fb4d099212ed5fb3327db8b66a02
Matt Mendell <matthew.mendell@gmail.com>
2011-09-05 19:29:58 -0700

    add button to root page on top view

--- apps/gighub/views/top.js
+++ apps/gighub/views/top.js
@@ -11,14 +11,16 @@ Gighub.TopView = SC.ToolbarView.extend(
 /** @scope Gighub.TopView.prototype */ {
 
     layout: { top: 0, left: 0, right: 0, height: 36 },
-    childViews: 'labelView bandsButton venuesButton nameView logoutButton'.w(),
+    childViews: 'rootView bandsButton venuesButton nameView logoutButton'.w(),
     anchorLocation: SC.ANCHOR_TOP,
 
-    labelView: SC.LabelView.design({
-        layout: { centerY: 0, height: 24, left: 8, width: 200 },
+    rootView: SC.ButtonView.design({
+        layout: { centerY: 0, height: 24, left: 8, width: 100 },
         controlSize: SC.LARGE_CONTROL_SIZE,
         fontWeight: SC.BOLD_WEIGHT,
-        value: 'gighub'
+        title: 'gighub',
+        target: 'Gighub.routes',
+        action: 'root'
     }),
 
     bandsButton: SC.ButtonView.design({
