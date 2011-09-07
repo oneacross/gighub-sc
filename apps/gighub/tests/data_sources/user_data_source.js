6617ebcb1b46716902583a1330b1c83a50d577bb
Matt Mendell <matthew.mendell@gmail.com>
2011-09-06 22:18:11 -0700

    add test for Gighub.DataSource

---
+++ apps/gighub/tests/data_sources/user_data_source.js
@@ -0,0 +1,14 @@
+// ==========================================================================
+// Project:   Gighub.DataSource Unit Test
+// Copyright: ©2011 My Company, Inc.
+// ==========================================================================
+/*globals Gighub module test ok equals same stop start */
+
+module("Gighub.DataSource");
+
+test("test description", function() {
+    var expected = "test";
+    var result   = "test";
+    equals(result, expected, "test should equal test");
+});
+
