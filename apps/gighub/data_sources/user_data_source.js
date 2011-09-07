a613388affc487cdbb9542a2cd6c9ef8446d9802
Matt Mendell <matthew.mendell@gmail.com>
2011-09-06 22:17:39 -0700

    remove unused code

--- apps/gighub/data_sources/user_data_source.js
+++ apps/gighub/data_sources/user_data_source.js
@@ -12,25 +12,6 @@ Gighub.DataSource = SC.DataSource.extend(
 
     // Query support
     fetch: function(store, query) {
-        // Only support two types of user queries
-        // 1. is this name already in use?
-        //    -> only want a boolean response
-        // 2. is this name and password combo correct?
-        //    -> want the user object as a response (without password)
-        //if (query.get('query_type') == 1) {
-        //    SC.Request.getUrl('/users?name=' + query.get('name'))
-        //        .header({'Accept': 'application/json'}).json()
-        //        .notify(this, 'didFetchUser', store, query)
-        //        .send();
-        //    return YES;
-        //}
-        //else if (query.get('query_type') == 2) {
-        //    SC.Request.getUrl('/users')
-        //        .header({'Accept': 'application/json'}).json()
-        //        .notify(this, 'didFetchUser', store, query)
-        //        .send();
-        //    return YES;
-        //}
 
         SC.Request.getUrl('/bands')
             .header({'Accept': 'application/json'}).json()
@@ -49,60 +30,13 @@ Gighub.DataSource = SC.DataSource.extend(
         }
     },
 
-    didFetchUser: function(response, store, query) {
-        if (SC.ok(response)) {
-            store.loadRecords(Gighub.User, response.get('body').content);
-            store.dataSourceDidFetchQuery(query);
-        }
-        else {
-            store.dataSourceDidErrorQuery(query, response);
-        }
-    },
-
     // Record support
     retrieveRecord: function(store, storeKey) {
-      
-      // TODO: Add handlers to retrieve an individual record's contents
-      // call store.dataSourceDidComplete(storeKey) when done.
-      
-      return NO ; // return YES if you handled the storeKey
-    },
-    
-    createRecord: function(store, storeHash) {
-      
-      SC.Request.postUrl('/users')
-          .header({'Content-Type': 'application/json'}).json()
-          .notify(this, 'didCreateUser', store, storeHash)
-          .send(store.readDataHash(storeHash));
-      
-      return YES;
+        return NO;
     },
 
-    didCreateUser: function(response, store, storeHash) {
-        if (SC.ok(response)) {
-            var parser = /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/;
-            var url = parser.exec(response.header('Location'))[8];
-            store.dataSourceDidComplete(storeHash, null, url);
-        }
-        else {
-            store.dataSourceDidError(storeHash,response);
-        }
-    },
-    
-    updateRecord: function(store, storeKey) {
-      
-      // TODO: Add handlers to submit modified record to the data source
-      // call store.dataSourceDidComplete(storeKey) when done.
-
-      return NO ; // return YES if you handled the storeKey
-    },
-    
-    destroyRecord: function(store, storeKey) {
-      
-      // TODO: Add handlers to destroy records on the data source.
-      // call store.dataSourceDidDestroy(storeKey) when done
-      
-      return NO ; // return YES if you handled the storeKey
+    commitRecords: function() {
+        return NO;
     }
-  
-}) ;
+    
+});
