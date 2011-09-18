b922a355a69460d672b78c3c092bc64a938014af
Matt Mendell <matthew.mendell@gmail.com>
2011-09-17 23:34:53 -0700

    Add template view and simple css for band page.

--- apps/gighub/views/band.js
+++ apps/gighub/views/band.js
@@ -2,47 +2,15 @@
 // Project:   Gighub.BandView
 // Copyright: ©2011 My Company, Inc.
 // ==========================================================================
-/*globals Gighub */
 
-/** @class
-  @extends SC.View
-*/
-Gighub.BandView = SC.View.extend(
-/** @scope Gighub.BandView.prototype */ {
+Gighub.BandView = SC.View.extend({
+    layout: { top: 70 },
 
-    layout: { top: 36 },
     childViews: 'contentView'.w(),
-
-    contentView: SC.View.extend(SC.ContentDisplay, {
-        layout: { top: 0, height: 100 },
-
-        contentDisplayProperties: 'name location website'.w(),
-        contentBinding: 'Gighub.bandController.content',
-
-        classNames: ['band'],
-
-        render: function(context, firstTime) {
-            var name = '';
-            var location = '';
-            var website = '';
-            var content = this.get('content');
-
-            if (content != null) {
-                name = content.get('name');
-                location = content.get('location');
-                website = content.get('website');
-            }
-
-            context = context.begin('div').addClass('band-name')
-                             .push(name).end();
-            context = context.begin('div').addClass('band-location')
-                             .push(location).end();
-            context = context.begin('a').addClass('band-website')
-                             .attr('href', website)
-                             .push('website').end();
-
-            sc_super();
-        }
+    contentView: SC.TemplateView.extend({
+        templateName: "band",
+        nameBinding: 'Gighub.bandController.name',
+        locationBinding: 'Gighub.bandController.location',
+        websiteBinding: 'Gighub.bandController.website'
     })
-
 });
